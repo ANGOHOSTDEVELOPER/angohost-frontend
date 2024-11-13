f00e700d696d53f0972606ff170b6c1fa0cdfb8d
franciscojoaopedro <franciscojoaopedro1998@gmail.com>
2024-11-13 18:18:37 +0100

    fix bug cors

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,9 @@
 import axios from "axios"
 
 // const API_BASE_URL =  import.meta.env.VITE_API_PROD || "https://api2.angohost.ao/v1/api" old
-
 const API_BASE_URL =  import.meta.env.VITE_API_PROD || "https://api4.angohost.ao/v1/api"
 //const API_BASE_URL_LOCAL =  import.meta.env.VITE_API_PROD || "http://localhost:3334/v1/api"
 const api=axios.create({
-    baseURL:API_BASE_URL
+    baseURL: API_BASE_URL
 })
 export default api;
