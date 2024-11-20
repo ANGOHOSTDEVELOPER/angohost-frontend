0c6a7b0937b138ff2ca2495fdb16203434d0417c
franciscojoaopedro <franciscojoaopedro1998@gmail.com>
2024-11-20 18:54:42 +0100

    update api url

--- src/services/api_gpo.ts
+++ src/services/api_gpo.ts
@@ -1,10 +1,10 @@
 import axios from "axios"
 
 
-//const API_BASE_URL =  import.meta.env.VITE_API_PROD || "https://gpo.angohost.ao/api"
-const API_BASE_URL_LOCAL =  import.meta.env.VITE_API_PROD || "http://localhost:7000/api"
+const API_BASE_URL =  import.meta.env.VITE_API_PROD || "https://gpo.angohost.ao/api"
+//const API_BASE_URL_LOCAL =  import.meta.env.VITE_API_PROD || "http://localhost:7000/api"
 const apiGPO=axios.create({
-    baseURL:  API_BASE_URL_LOCAL,
+    baseURL:   API_BASE_URL,
 
 })
 export default apiGPO;
