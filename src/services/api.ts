0334af2e5f6ddba79f9899d85027bac5ea5610b4
franciscojoaopedro <franciscojoaopedro1998@gmail.com>
2025-01-08 11:50:36 +0100

    `Removed API key and withCredentials from axios instance configuration`

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,6 @@ import Cookies from "js-cookie"
 // const API_BASE_URL =  import.meta.env.VITE_API_PROD || "https://api2.angohost.ao/v1/api" old
 const API_BASE_URL =  import.meta.env.VITE_API_PROD || "https://api4.angohost.ao/v1/api"
 const API_BASE_URL_LOCAL =  "http://localhost:3334/v1/api"
-const apikey = import.meta.env.VITE_API_KEY
 
 const apiMode ={
     prod: API_BASE_URL,
@@ -12,9 +11,7 @@ const apiMode ={
 }
 const api = axios.create({
     baseURL: apiMode.prod,
-    headers: {
-        "x-api-key": apikey}
-        ,withCredentials: true
+   
 })
 
 
